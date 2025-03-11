5f5d7a9a50c53190b8db957f6897d70f42856780
Keneo25 <Koni2835@interia.eu>
2025-03-11 18:57:25 +0100

    fix2

--- ConsoleApp1/IMAP_Logic.cs
+++ ConsoleApp1/IMAP_Logic.cs
@@ -6,26 +6,34 @@ using MimeKit;
 using Org.BouncyCastle.Tls;
 
 namespace ConsoleApp1;
-
 public class ImapLogic
 {
-    public void Run()
+    private string Host { get; }
+    private string Username { get; }
+    private string Password { get; }
+    private int Port { get; }
+    
+    public ImapLogic()
     {
-        using var client = new ImapClient();
-        
-        string host = Environment.GetEnvironmentVariable("IMAP_HOST") ?? throw new InvalidOperationException("Brak IMAP_HOST");
-        string username = Environment.GetEnvironmentVariable("IMAP_USERNAME") ?? throw new InvalidOperationException("Brak IMAP_USERNAME");
-        string password = Environment.GetEnvironmentVariable("IMAP_PASSWORD") ?? throw new InvalidOperationException("Brak IMAP_PASSWORD");
-        bool success = int.TryParse(Environment.GetEnvironmentVariable("IMAP_PORT"), out int port);
-        if (success == false)
+        Host =  Environment.GetEnvironmentVariable("IMAP_HOST") ?? throw new InvalidOperationException("Brak IMAP_HOST");
+        Username = Environment.GetEnvironmentVariable("IMAP_USERNAME") ?? throw new InvalidOperationException("Brak IMAP_USERNAME");
+        Password = Environment.GetEnvironmentVariable("IMAP_PASSWORD") ?? throw new InvalidOperationException("Brak IMAP_PASSWORD");
+        if (!int.TryParse(Environment.GetEnvironmentVariable("IMAP_PORT"), out int port))
         {
             throw new InvalidOperationException("Brak IMAP_PORT");
         }
-        client.Connect(host, port, true);
-        client.Authenticate(username,password);
+        Port = port;
+    }
 
+    public void Connect(ImapClient client)
+    {
+        client.Connect(Host, Port, true);
+        client.Authenticate(Username,Password);
+    }
+
+    public void CheckOrCreateFolder(ImapClient client)
+    {
         var inbox = client.Inbox;
-        
         inbox.Open(FolderAccess.ReadWrite);
         var x = inbox.GetSubfolders().ToList();
         IMailFolder? destination = null;
@@ -43,6 +51,13 @@ public class ImapLogic
             CheckForNewMessages(inbox, destination);
             Thread.Sleep(TimeSpan.FromMinutes(1));
         }
+    }
+    
+    public void Run()
+    {
+        using var client = new ImapClient();
+        Connect(client);
+        CheckOrCreateFolder(client);
     }
     private void CheckForNewMessages(IMailFolder inbox, IMailFolder destination)
     {
@@ -72,19 +87,19 @@ public class ImapLogic
     
     private void SaveAttachmentFromMessage(List<MimeEntity> attachments)
     {
-        string path = "D:\\Pojekty\\Projek_Z_Trans\\fv";
+        var path = "D:\\Pojekty\\Projek_Z_Trans\\fv";
         Directory.CreateDirectory(path);
 
         foreach (var attachment in attachments.OfType<MimePart>())
         {
-            string name = attachment.ContentDisposition?.FileName ?? $"unknown_{Guid.NewGuid()}";
-            string pathFile = Path.Combine(path, name);
-            int counter = 1;
+            var name = attachment.ContentDisposition?.FileName ?? $"unknown_{Guid.NewGuid()}";
+            var pathFile = Path.Combine(path, name);
+            var counter = 1;
 
             while (File.Exists(pathFile))
             {
-                string ex = Path.GetExtension(name);
-                string filename = Path.GetFileNameWithoutExtension(name);
+                var ex = Path.GetExtension(name);
+                var filename = Path.GetFileNameWithoutExtension(name);
                 pathFile = Path.Combine(path, $"{filename}({counter++}){ex}");
             }
             using var stream = File.Create(pathFile);
